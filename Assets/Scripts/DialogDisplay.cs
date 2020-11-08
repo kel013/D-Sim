edc081b28b3437fdc26c94133924224749151fd4
Static Void <klinye@usc.edu>
2020-11-08 00:36:25 -0800

    talkable

---
+++ Assets/Scripts/DialogDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogDisplay : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text text;
+    [SerializeField]
+    float perCharacterPauseTime = 0.1f;
+    int currentLine;
+    bool talking;
+    public void DisplayDialog(string[] line)
+    {
+        currentLine = 0;
+        StartCoroutine(StartDialog(line));
+    }
+    IEnumerator StartDialog(string[] line)
+    {
+        talking = true;
+        while (currentLine < line.Length)
+        {
+            text.text = line[currentLine];
+            yield return new WaitForSeconds(1f + line[currentLine].Length * perCharacterPauseTime);
+            currentLine++;
+        }
+        talking = false;
+        text.text = "";
+    }
+}
---
+++ Assets/Scripts/Talkable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talkable : Interactable
+{
+    [SerializeField]
+    DialogDisplay display;
+    [SerializeField]
+    Dialog dialog;
+    public override void Interact()
+    {
+        display.DisplayDialog(dialog.lines);
+    }
+}
