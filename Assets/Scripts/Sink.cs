8ffb7f2bc06dfd50d8ed2f338ab5e3aa0af35349
bethany <bethany@usc.edu>
2020-11-09 04:05:25 +0800

    More animations

--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Sink : Interactable
 {
+    [SerializeField] Animation anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     public override void Interact()
     {
+        if (anim.isPlaying)
+            return;
+
+        anim.Play("Take 001");
         linkedTask.done = true;
     }
 }
