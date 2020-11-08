b4ecbf9f58473bc2f8bff6d48ca5d47804ea6c1f
Static Void <klinye@usc.edu>
2020-11-08 12:50:09 -0800

    can interact with doors

---
+++ Assets/Scripts/Pushable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushable : Interactable
+{
+    GameObject player;
+    float force = 5;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("MainCamera");
+        rb = GetComponent<Rigidbody>();
+    }
+    public override void Interact()
+    {
+        Debug.Log("dothing");
+        rb.velocity = player.transform.forward * force;
+    }
+}
